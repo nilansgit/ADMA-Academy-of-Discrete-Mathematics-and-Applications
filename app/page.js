9989ceab0d75e8935f23d87153f63b16e9f61c7a
Nilansh <nilansh.nk@gmail.com>
2025-10-24 18:01:14 +0530

    dfdfdff

--- app/page.js
+++
@@ -1,7 +0,0 @@
-// app/page.js
-export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/hello");
-  const data = await res.json();
-
-  return <h1>{data.message}</h1>;
-}
