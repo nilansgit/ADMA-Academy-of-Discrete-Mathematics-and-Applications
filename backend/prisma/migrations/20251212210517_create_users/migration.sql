1ce0808358ad1f82f0d6cf542a89b405e13d4840
Nilansh <nilansh.nk@gmail.com>
2025-12-13 02:49:25 +0530

    db-setup

---
+++ backend/prisma/migrations/20251212210517_create_users/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `role` ENUM('admin', 'treasurer', 'secretary') NULL,
+    `created_at` TIMESTAMP(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
