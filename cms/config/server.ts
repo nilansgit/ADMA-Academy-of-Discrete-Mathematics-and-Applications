36df09194b990b850faf151e493fcc071035a090
Nilansh <nilansh.nk@gmail.com>
2026-02-02 11:21:07 +0530

    cms fix

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -2,6 +2,7 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: 'http://10.17.0.140/cms',
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
