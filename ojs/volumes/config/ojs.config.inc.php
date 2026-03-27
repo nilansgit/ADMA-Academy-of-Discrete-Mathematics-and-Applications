1a0a8973022c764e50aa6c126cd4a4b2f4b292bf
Nilansh <nilansh.nk@gmail.com>
2026-03-27 20:00:15 +0530

    remove s from http

--- ojs/volumes/config/ojs.config.inc.php
+++ ojs/volumes/config/ojs.config.inc.php
@@ -29,7 +29,7 @@
 installed = Off
 
 ; The canonical URL to the OJS installation (excluding the trailing slash)
-base_url = "https://pkp.sfu.ca/ojs"
+base_url = "http://pkp.sfu.ca/ojs"
 
 ; Session cookie name
 session_cookie_name = OJSSID
